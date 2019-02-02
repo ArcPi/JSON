cafd0e291d1d319dac0f5caa1fb37d45913c269b
arcpi <shenmeisheng1995@gmail.com>
2019-02-02 22:31:26 +0800

    complete chapter 01

--- leptjson.c
+++ leptjson.c
@@ -68,7 +68,7 @@ int lept_parse(lept_value* v, const char* json){
     c.json = json;
     v->type = LEPT_NULL;
     lept_parse_whitespace(&c);
-    if((ret == lept_parse_value(&c, v)) == LEPT_PARSE_OK){
+    if((ret = lept_parse_value(&c, v)) == LEPT_PARSE_OK){
         lept_parse_whitespace(&c);
         if(*c.json != '\0')
             ret = LEPT_PARSE_ROOT_NOT_SINGULAR;
